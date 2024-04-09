8c3019dac433fbbb963b3f9eda0760e07fffdfd8
AxelHenir <axelhenirmail@gmail.com>
2024-04-09 00:17:00 -0400

    Pages in place

--- grid.js
+++ grid.js
@@ -19,18 +19,11 @@ const PALETTES = [
     ["#FBF8CC","#FDE4CF","#FFCFD2","#F1C0E8","#CFBAF0","#A3C4F3","#90DBF4","#8EECF5","#98F5E1","#B9FBC0",],
     // Synthwave
     ["#FFBE0B","#FB5607","#FF006E","#8338EC","#3A86FF",],
-    // Coffee
-    ["#EDE0D4","#E6CCB2","#DDB892","#B08968","#7F5539",],
     // Red n Blue
     ["#e63946", "#f1faee", "#a8dadc", "#457b9d", "#1d3557"],
-    // Forest
-    ["#e9f5db", "#cfe1b9", "#b5c99a", "#97a97c", "#87986a", "#718355"],
     // Heatwave
     ["#ff7b00", "#ff8800", "#ff9500", "#ffa200", "#ffaa00", "#ffb700", "#ffc300", "#ffd000", "#ffdd00", "#ffea00"],
-    // Reptile
-    ["#05668d","#028090","#00a896","#02c39a","#f0f3bd"],
-    // Purp
-    ["#240046","#3c096c","#5a189a","#7b2cbf","#9d4edd","#c77dff","#e0aaff"],
+    
 
     
 ];
@@ -38,7 +31,7 @@ const PALETTES = [
 // Sets up the canvas and calls for a new diagram to be drawn
 function setup(){
 
-    c = createCanvas(2000,750);
+    c = createCanvas(visualViewport.width,visualViewport.height*0.75);
     c.parent("canvasContainer");
 
     newDiagram();
@@ -83,11 +76,20 @@ function Cell(rows,cols,width,height,pal){
         if(this.w > 8){
 
             push();
+            
             rectMode(CORNER);
             fill(random(this.palette));
 
-            // stroke(255);
-            // strokeWeight(1);
+            let rnd = random();
+            if (rnd > 0.75){
+                stroke('white');
+                strokeWeight(1);
+            } else if (rnd > 0.5){
+                stroke('black');
+                strokeWeight(1);
+            } else {
+                noStroke();
+            }
             
             // Draw the BG of the currnt cell
             rect(0,0,this.w,this.h,0);
@@ -100,8 +102,16 @@ function Cell(rows,cols,width,height,pal){
                 ellipseMode(CORNER);
                 fill(random(this.palette));
 
-                // stroke(255);
-                // strokeWeight(1);
+                let rnd = random();
+                if (rnd > 0.75){
+                    stroke('white');
+                    strokeWeight(1);
+                } else if (rnd > 0.5){
+                    stroke('black');
+                    strokeWeight(1);
+                } else {
+                    noStroke();
+                }
 
                 // Draw the circle
                 ellipse(0,0,this.w,this.h);
